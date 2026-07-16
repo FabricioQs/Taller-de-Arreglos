9a6ce3f2fa3c0f6ee64f4f0604963a70bb4ed8bf
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-07-16 18:24:52 -0500

    Paso 2 completado: agregarEdad funcional

--- arreglos1.js
+++ arreglos1.js
@@ -1,2 +1,8 @@
 let edadLadoIzquierdo = [];
 let edadLadoDerecha = [];
+
+function agregarEdad (){
+    let edad = recuperarInt("edad");
+    edadLadoIzquierdo.push(edad);
+    pintarArregloIzquierda();
+}
