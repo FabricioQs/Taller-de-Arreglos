ab4c8d9c8495d0cae2cbc5902a019257dda43fb8
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-07-16 18:42:17 -0500

    Paso 3: Funcion pintarArregloIzquierda mostrando la edad

--- arreglos1.js
+++ arreglos1.js
@@ -1,8 +1,25 @@
-let edadLadoIzquierdo = [];
+let edadLadoIzquierdo = []; //declaramos los arreglos
 let edadLadoDerecha = [];
 
 function agregarEdad (){
-    let edad = recuperarInt("edad");
-    edadLadoIzquierdo.push(edad);
-    pintarArregloIzquierda();
+    let edad = recuperarInt("edad"); //utilizamos utilitarios para recuperar la id edad en html caja de texto
+    edadLadoIzquierdo.push(edad); //guardamos con push el arreglo Izquierdo
+    pintarArregloIzquierda(); //llama a la funcion
+}
+
+function pintarArregloIzquierda() {
+
+    let cmpTabla = document.getElementById("tablaIzquierda"); //recuperamos el cuerpo de la tabla en html usando su id
+    
+    let contenidoTabla = ""; //preparamos una variable vacia 
+
+    for (let i = 0; i < edadLadoIzquierdo.length; i++) { //recorremos nuestro arreglo
+
+        let miEdad = edadLadoIzquierdo[i]; //guardamos la edad actual en una variable
+
+        contenidoTabla += "<tr><td>"; 
+        contenidoTabla += miEdad;       //creamos la tabla segun la edad que pusimos y concatenamos
+        contenidoTabla += "</td></tr>";
+    }
+    cmpTabla.innerHTML = contenidoTabla; //metemos todo el texto dentro del html
 }
